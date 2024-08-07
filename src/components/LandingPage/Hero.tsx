0cf37626e38bf79f00c3e913d54517beec4c8728
IjonK007 <kushta.joni@gmail.com>
2024-08-07 18:27:39 +0200

    Dot bg made smaller

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -50,7 +50,7 @@ export default function Hero() {
         cy={1}
         cr={1}
         className={cn(
-          "[mask-image:radial-gradient(2000px_circle_at_center,white,transparent)]",
+          "[mask-image:radial-gradient(1400px_circle_at_center,white,transparent)]",
         )}
       />
     </div>
